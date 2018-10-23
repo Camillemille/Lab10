cc449becb7e7d35d307c5ac30ec39e1e1c0afcbc
Camillemille <42758821+Camillemille@users.noreply.github.com>
2018-10-23 15:34:24 -0400

    lab10

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Circle mycircle = new Circle(20);
+
+            Console.WriteLine("Please enter the radius of a circle!");
+           double UserInput = double.Parse(Console.ReadLine());
+
+
+            Circle myCircle = new Circle(UserInput);
+
+            Console.WriteLine($" Circumference: {myCircle.CalculateCircumference()}");
+            Console.WriteLine($"Circumference Formatted! { myCircle.CalculateFormattedCircumference()}" );
+
+            Console.WriteLine($"Area: {mycircle.CalculateArea()}");
+            Console.WriteLine($"Area: {mycircle.CalculateFormattedArea()}");
+           
+
+
+
+        }
+    }
+}
